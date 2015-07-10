438e97228678e74110320485cdd781a0d7f01163
benneb2 <benneb2@gmail.com>
2015-07-10 14:18:03 +0200

    Added flip

---
+++ packages/1.0/js/flip.js
@@ -0,0 +1,57 @@
+_flip = {
+
+    model : [ ],
+    
+    onExit : function() { var _ = this;
+
+    },
+
+    onLoaded: function () { var _ = this;
+
+
+    	layout.attach('#flipFront');
+    	layout.attach('#flipBack');
+
+    	_flip.model =
+    	{
+    	 	dir : "rl",
+    	}
+
+    	setTimeout(
+			function() {
+				_flip._Ctrl();  
+			}
+			, 1000);
+    	
+    },
+
+    onMessage : function() {
+
+
+    },
+
+    Ctrl : function($scope)
+    {
+    	 $scope.data = _flip.model;
+    },
+
+    _Ctrl : function()
+  	{
+	    e = document.getElementById('flipFront__FACE');
+	    
+	    scope = angular.element(e).scope();
+	    
+	    scope.$apply(function() 
+	    {  
+	        scope.data = _flip.model;
+	    }); 
+    },
+    setDir : function(dir)
+    {
+		$('#flip').attr('flip',dir);
+    	_flip.model.dir = dir;
+    	_flip._Ctrl();
+    }
+
+
+};;;
