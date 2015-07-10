02c3764e0de66e764e542d52b8d764c7e68aa757
benneb2 <benneb2@gmail.com>
2015-07-10 13:04:46 +0200

    Added Api Request

---
+++ packages/1.0/js/apiReq.js
@@ -0,0 +1,74 @@
+_apiReq = {
+
+    model : [ ],
+    
+    onExit : function() { var _ = this;
+
+    },
+
+    onLoaded: function () { var _ = this;
+
+
+    	 layout.attach('#apiReqFront');
+
+ 		 _apiReq.model = 
+         {
+            apiReq : undefined,
+            apiSend : "{some:'data', is:'more',equal:'than',other:'data'}",
+         }
+
+		setTimeout(
+			function() {
+				_apiReq._Ctrl();  
+			}
+			, 1000);
+    	
+    },
+
+    onMessage : function() {
+
+
+    },
+
+    Ctrl : function($scope)
+    {
+		$scope.data = _apiReq.model
+    },
+
+    _Ctrl : function()
+  	{
+	    e = document.getElementById('apiReqFront__FACE');
+	    
+	    scope = angular.element(e).scope();
+	    
+	    scope.$apply(function() 
+	    {  
+	        scope.data = _apiReq.model;
+	    }); 
+    },
+
+  sendApiReq : function() {
+		
+		 api.req("apiReq",
+		 {
+		 	data: _apiReq.model.apiSend
+         },
+         function(response)
+         {
+
+	         if(!response)
+	           {
+	                   //request returned nothing
+	                   alert("Nothing Returned from API Request");
+	           }
+	       else
+	           {
+	              _apiReq.model.apiReq = response;
+	              _apiReq.model.apiSend = '';
+	              _apiReq._Ctrl();
+	           }
+         });
+		}
+  	
+
+};;;
