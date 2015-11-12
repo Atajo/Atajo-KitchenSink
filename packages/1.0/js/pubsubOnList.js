a06099b1134596d61ffc5a7fec77c87da86f4ff6
Ann Jose <ann.jose@britehouse.co.za>
2015-11-12 16:58:51 +0200

    jobs for all items in a list via pubsub

--- packages/1.0/js/pubsubOnList.js
+++ packages/1.0/js/pubsubOnList.js
@@ -7,6 +7,8 @@ _pubsubOnList = {
     lastFace : 0,
     info : [],
     status:"NONE",
+    statuses:[],
+    multi:false,
     
     onExit : function() { var _ = this;
 
@@ -45,6 +47,8 @@ _pubsubOnList = {
                 {
 
                     _pubsubOnList.currJob = {status:status,jobName:job.jobName};
+                    _pubsubOnList.statuses[i] = status;
+
 
                     layout.attach('#pubsubOnListFront');
                 }
@@ -62,17 +66,35 @@ _pubsubOnList = {
     },
 
     pubCtrl : function($scope) {
+
         $scope.info = _pubsubOnList.info;
 
         $scope.data = _pubsubOnList.model;
         $scope.currJob = _pubsubOnList.currJob;
 
-        if ($scope.index != null) {
-            $scope.info[$scope.index].status = $scope.currJob.status;
+
+        for (var j = 0; j < (($scope.info).length); j++) {
+            //alert("$scope.info[j].status : "+ $scope.info[j].status);
         }
 
 
-        $scope.sendJob = function (code,name,description,allowDuplicate,clearOnDone,item,index,lockUI,cb) {
+      if (($scope.index != null) && _pubsubOnList.multi)
+      {
+         for (var j = 0; j < (($scope.info).length); j++) {
+             $scope.info[j].status = _pubsubOnList.statuses[j];
+         }
+      }
+
+        if (($scope.index != null) && !(_pubsubOnList.multi))
+        {
+            $scope.info[$scope.index].status = _pubsubOnList.currJob.status;
+        }
+
+
+
+            $scope.sendJob = function (code,name,description,allowDuplicate,clearOnDone,item,index,lockUI,cb) {
+
+                _pubsubOnList.multi = false;
 
             if(typeof code == 'undefined') {  code = 0; }
             if(typeof name == 'undefined') {  name = "JOB NAME " + code; }
@@ -111,6 +133,53 @@ _pubsubOnList = {
 
                 alert(err);
 
+            }
+
+        }
+
+        $scope.sendJobs = function (code,name,description,allowDuplicate,clearOnDone,items,lockUI,cb) {
+
+            _pubsubOnList.multi = true;
+
+            if(typeof code == 'undefined') {  code = 0; }
+            if(typeof name == 'undefined') {  name = "JOB NAME " + code; }
+            if(typeof description == 'undefined') {  description = "JOB DESC "; }
+            if(typeof allowDuplicate == 'undefined') {  allowDuplicate = false; }
+            if(typeof clearOnDone == 'undefined') {  clearOnDone = false; }
+            if(typeof lockUI == 'undefined') {  lockUI = false; }
+            if(typeof cb == 'undefined') {  cb = false; }
+
+
+            for(var k=0;k<((items).length);k++) {
+
+                $scope.index = k;
+
+                try {
+
+                    response = {code: code, req: "req", res: "res", msg: "msg"};
+
+                    jobData = {action: 'jobQueue', data: response};
+
+                    JOB = {
+                        jobName: name,
+                        jobDesc: description,
+                        data: jobData,
+                        allowDuplicate: allowDuplicate,
+                        clearOnDone: clearOnDone,
+                        lockUI: lockUI,
+                        callback: cb
+                    };
+
+                    _log.d("JOB = " + JSON.stringify(JOB));
+                    jobid = jobQueue.add(JOB);
+                    _pubsubOnList.currJobs.push({jobID: jobid, jobName: name, jobData: items[k]});
+                    layout.attach('#pubsubOnListFront');
+
+                } catch (err) {
+
+                    alert(err);
+
+                }
             }
 
         }
